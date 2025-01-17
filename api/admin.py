cbbac8db711866c1b71ca63bb20973c712a2093a
Sachin Vardhan <vardhansachin08@gmail.com>
2025-01-17 14:25:30 +0530

    Changes

--- api/admin.py
+++ api/admin.py
@@ -1,20 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Subscription
-from django.utils import timezone
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
-    
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
-    
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
