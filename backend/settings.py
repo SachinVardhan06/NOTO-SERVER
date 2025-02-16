3d620ec96f96b0bcdf5eac489871053a0168dbd8
Sachin Vardhan <vardhansachin08@gmail.com>
2025-02-16 16:13:40 +0530

    new

--- backend/settings.py
+++ backend/settings.py
@@ -411,12 +411,12 @@ LOGGING = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "https://acenoto.tech/",
-]
+# # CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:5174",
+#     "http://127.0.0.1:5173",
+#     "http://127.0.0.1:5174",
+#     "https://acenoto.tech/",
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
